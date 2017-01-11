d42012ee7c4dac55b92fa0738b9ae808656eda57
Aviv5374 <Aviv5374@gmail.com>
2017-01-11 11:00:36 +0200

    home work on PHP with AJAX 1

--- year 3/lesson11 - PHP with AJAX/Client1.php
+++ year 3/lesson11 - PHP with AJAX/Client1.php
@@ -1,31 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-<script>
-function ajaxHint(str){
-	if(str.length == 0){
-		document.getElementById("options").innerHTML = "";
-		return;
-	} else {
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function () {
-			if (this.readyState == 4 && this.status == 200) {
-				document.getElementById("options").innerHTML = this.responseText;
+	<title>Hint1</title>
+	<script>
+		function ajaxHint(str){
+			if(str.length == 0){
+				document.getElementById("options").innerHTML = "";
+				return;
+			} else {
+				var xmlhttp = new XMLHttpRequest();
+				xmlhttp.onreadystatechange = function () {
+					if (this.readyState == 4 && this.status == 200) {
+						document.getElementById("options").innerHTML = this.responseText;
+					}
+				};
+				xmlhttp.open("GET","hint1.php?q=" + str, true);
+				xmlhttp.send();
 			}
-		};
-		xmlhttp.open("GET","hint1.php?q=" + str, true);
-		xmlhttp.send();
-	}
-}
-</script>
+		}
+	</script>
 </head>
 <body>
-<fieldset>
-	<input type="search" name="keyword" onkeyup="ajaxHint(this.value)" placeholder="looking for" autofocus>
-	<div id="options"></div>
-</fieldset>
-
-
-
+	<fieldset>
+		<input type="search" name="keyword" onkeyup="ajaxHint(this.value)" placeholder="looking for" autofocus>
+		<div id="options"></div>
+	</fieldset>
 </body>
 </html>
