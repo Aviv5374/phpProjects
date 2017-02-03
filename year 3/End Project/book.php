f26d89ad27a441cbec35463c147278f06fea47cb
Aviv5374 <Aviv5374@gmail.com>
2017-02-03 14:18:08 +0200

    end project 8 setup book.php 3

--- year 3/End Project/book.php
+++ year 3/End Project/book.php
@@ -2,7 +2,7 @@
 session_start();
 include 'db.php';
 define("KILOBYTE", 1000);
-define("MEGABYTE", ("KILOBYTE"*"KILOBYTE"));
+define("MEGABYTE", (KILOBYTE*KILOBYTE));
 define("GIGABYTE", ("KILOBYTE"*"MEGABYTE"));
 
 
@@ -11,56 +11,82 @@ if(empty($_SESSION) or isset($_POST['logout'])){
 	echo "Goodbye";
 	exit();
 }
-else if (isset($_POST['UploadImage']) and ) {
-	if (isset($_FILES['fileToUpload']) and isset($_POST['title'])) {
-		$file_path = "uploads/";
-
-     if ($_FILES['fileToUpload']['size'] > 35*MEGABYTE) //check size
-     {
-     	header("refresh:2; url=$_SERVER['PHP_SELF']");
-     	echo "Your File is too Large";
-     	exit();
-     }
-
-
-     $file_path = $file_path . basename($_FILES['fileToUpload']['name']);
-
-     $file_ext = pathinfo($file_path,PATHINFO_EXTENSION);
-     $expensions = array("jpeg", "jpg", "png");
-     if (!in_array($file_path, $expensions))// check  
-     {
-     	header("refresh:2; url=$_SERVER['PHP_SELF']");
-     	echo "Sorry, Only JPEG/JPG/PNG Files are allowed";
-     	exit();     
-     }
-
-	if (file_exists($file_path))// check if the file already exists
+else if (isset($_POST['UploadImage'])) 
+{
+	if (isset($_FILES['fileToUpload']) and isset($_POST['title'])) 
 	{
-		header("refresh:2; url=$_SERVER['PHP_SELF']");
-		echo "File already exists";
-		exit();
-	}
+       if ($_FILES['fileToUpload']['size'] > 100*MEGABYTE) //check size
+       {
+       	header("refresh:2; url=".$_SERVER['PHP_SELF']."");
+       	echo "Your File is too Large";
+       	exit();
+       }
 
-	$title = mysqli_real_escape_string($con,$_POST['title']);
+       $file_path = "uploads/";
+       $file_path = $file_path . basename($_FILES['fileToUpload']['name']);
 
-	if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $file_path)){
-		$sql = "INSERT INTO book VALUE (null, '$file_path', 'title', null)";
-		if (mysqli_query($con, $sql)) {
-			echo "success";
-		}
-		else echo "SQL Fail";
+       $file_ext = pathinfo($file_path,PATHINFO_EXTENSION);
+       $expensions = array("jpeg", "jpg", "png");
+       if (!in_array($file_path, $expensions))// check  
+       {
+       	header("refresh:2; url=".$_SERVER['PHP_SELF']."");
+       	echo "Sorry, Only JPEG/JPG/PNG Files are allowed";
+       	exit();     
+       }
+
+	   if (file_exists($file_path))// check if the file already exists
+	   {
+	   	header("refresh:2; url=".$_SERVER['PHP_SELF']."");
+	   	echo "File already exists";
+	   	exit();
+	   }
+
+	   if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $file_path))
+	   {
+	   	$title = mysqli_real_escape_string($con,$_POST['title']);
+	   	$sql = "INSERT INTO book VALUE (null, '$file_path', 'title', null)";
+	   	if (mysqli_query($con, $sql)) 
+	   	{
+	   		echo "success";
+	   		?>
+	   		<!DOCTYPE html>
+	   		<html>
+	   		<head>
+	   			<script type="text/javascript">
+	   				function CreateNewImageDiv() {
+	   					var Book = document.getElementById('Book');
+
+	   					var NewImageDiv = document.createElement("div");
+	   					var att = document.createAttribute("class");
+	   					att.value = "Image";
+	   					NewImageDiv.setAttributeNode(att);
+
+	   					var NewImageSurce = document.createElement("img");
+	   					NewImageSurce.src = <?php echo $file_path ?>;
+	   					NewImageDiv.appendChild(NewImageSurce);
+
+	   					var NewImageTitel = document.createElement("p");
+	   					NewImageTitel.text = <?php echo $title ?>;
+	   					NewImageDiv.appendChild(NewImageTitel);
+
+	   					Book.appendChild(NewImageDiv);	
+	   				}
+
+	   				CreateNewImageDiv();
+	   			</script>
+	   		</head>
+	   		</html>
+	   		<?php
+	   	}
+	   	else echo "SQL Fail";
+	   }
+	   else echo "fail"; 
 	}
-	else{
-		echo "fail";
-	}
+	else echo "Missing Variables";
 }
-else echo "Missing Variables";
-
-}
-
-
 
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -69,7 +95,7 @@ else echo "Missing Variables";
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript">
 		function ajaxHint(str) {
-			if (str.length == 0) {
+			/*if (str.length == 0) {
 				//code...
 				return;
 			}else{
@@ -80,7 +106,7 @@ else echo "Missing Variables";
 					}
 				};
 				xmlhttp.open("POST","upload.php",true);
-				xmlhttp.send();
+				xmlhttp.send();*/
 
 			}
 		}
